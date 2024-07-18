08e16667e4c7be56bc59212cdaca2ccf511c66a3
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-07-18 18:25:39 -0500

    Setting up prisma adapter, and the schema in the db

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,11 +1,14 @@
 import { NuxtAuthHandler } from "#auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 
 const { AUTH_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } =
 	useRuntimeConfig();
 
 export default NuxtAuthHandler({
 	secret: AUTH_SECRET,
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		// @ts-expect-error
 		GithubProvider.default({
